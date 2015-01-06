6c601e96170d96e08ae40e03693b70da1e473568
raymondphua <chrphua@student.avans.nl>
2015-01-06 14:51:55 +0100

    .net 4.5 ipv 4.0

---
+++ PROG6_Assessment/PROG6_Assessment/Model/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PROG6_Assessment.Model
+{
+    public interface IProductRepository
+    {
+        List<Product> ToList();
+    }
+}
---
+++ PROG6_Assessment/PROG6_Assessment/ViewModel/ProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using PROG6_Assessment.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PROG6_Assessment.ViewModel
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        private Product _product;
+
+        public ProductViewModel()
+        {
+            this._product = new Product();
+        }
+
+        public ProductViewModel(Product product)
+        {
+            this._product = product;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
