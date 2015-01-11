01a4b12de4abfbe415fe32b2b32d31ed190b7a33
raymondphua <chrphua@student.avans.nl>
2015-01-11 16:27:56 +0100

    nu wel de schermen?

---
+++ PROG6_Assessment/DomainModel/Model/Korting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Model
+{
+    public class Korting
+    {
+        [Key]
+        public int KortingId { get; set; }
+        public string Coupon { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
